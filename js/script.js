d706f2b7d3455d319d73c825ecf2e471d75d8102
Pedro Henrique Dias de Souza <desenvolvedorweb7070@hotmail.com>
2026-06-11 13:46:01 -0300

    Upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Rest Operator
+
+// function convidados(...nomes) {
+//     console.log("Seja bem vindo");
+//     console.log(nomes)
+// }
+
+// convidados("Lucas","João","Marcos", "Mario Silva");
+
+
+function  soteador(...numeros) {
+   console.log(numeros);
+
+   const numeroGerado = Math.floor(Math.random() * numeros.length );
+
+   console.log("Numero gerado foi:" + numeros[numeroGerado]);
+}
+
+soteador(1,4,5,15,25,90,55,34);
